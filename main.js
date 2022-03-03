91e75eb4997837cffa568847ec84e28a2662172f
silmu <svetlana.raitina@gmail.com>
2022-03-03 10:21:25 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//colors appear at random after you click Start
+//one highlighted button cannot be hightlighted twice in a row
+//four circles (each has it's own color ideally)
+//the player should click the circle before the circle changes
+//when you have game over a modal window appears that says the score and game over
+//
+//when the game just starts the first round lets you skip first clicks ?
+//the speed of the game increases
+//circles are not clickable before the game starts
+//
+//next level: you have only Start game button. After it's clicked it's changed to Stop game button. - Hide one button with JS basically
+//
+//after game starts it let's you make 3 misclicks
+// add sounds on start and end game
+// change message of modal depending on the score
+//
+//other versions: circles lit up and you have to repeat the pattern before timer runs out;
+//you have to repeat the patter and the game is waiting, patterns are getting longer each time.
+//
+//advanced: player can enter their name and the end result will be saved in JSON format
+//top 10 scores are shown at the end of the game
+//game gets faster with each level or number of circles increases
